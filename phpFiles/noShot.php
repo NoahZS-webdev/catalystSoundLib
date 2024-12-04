2ff9d58ec8debb12a3f53f763458e128487301b9
NoahZS-webdev <noahzsisson@gmail.com>
2024-12-04 11:03:16 -0800

    PHP

---
+++ phpFiles/noShot.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Kill me</title>
+</head>
+
+<?php 
+
+echo"hello world!";
+//the following code is Noah goofing off. you can delete at any time
+
+?>
+</html>
