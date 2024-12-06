2cdd7f62934e80a820d6257f2150064e658b0c81
NoahZS-webdev <noahzsisson@gmail.com>
2024-12-06 09:54:03 -0800

    more php tweaks

---
+++ basicallyaterminal.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Trying to get php to work</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<?php 
+
+//Log into server
+$servername = "localhost";
+$username = "noahzs";
+$password = "1999mazdarx-7";
+$dbname = "audio_db";
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed ". $conn->connect_error);
+}else{
+echo"Connected Successfully<br><br>";
+}
+
+
+// $sql = "CREATE TABLE audioData (
+//     fileID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//     dispName VARCHAR(50) NOT NULL,
+//     author VARCHAR(50) NOT NULL,
+//     fileType ENUM('mp3','wav') NOT NULL
+//     )";
+
+// $sql = "SELECT * FROM audioData";
+$sql = "INSERT INTO audioData (dispName, author, fileType)
+VALUES ('PLEASE WORK FOR THE LOVE OF GOD', 'a silly lil guy', 'wav')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Some shit was successful idk man i just work here";
+}else{
+    echo "Oh wow i sure hope my sql works. Fuck you. said the sql who was actually the fuck you guy.<br><br>Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+?>
+</body>
+</html>
