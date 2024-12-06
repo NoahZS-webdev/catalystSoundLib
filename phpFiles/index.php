054b44145006474aef5042078b6dc7d8e3422455
NoahZS-webdev <noahzsisson@gmail.com>
2024-12-06 09:46:59 -0800

    php stuff

--- phpFiles/index.php
+++ phpFiles/index.php
@@ -1,31 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Kill me</title>
-</head>
-
 <?php 
 
-echo"hello world";
+//Log into server
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
-// //Log into server
-// $servername = "localhost";
-// $username = "root";
-// $password = "2006VWjetta2.5L5cyl";
-// $dbname = "audiodb";
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// //create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// //check connection
-// if ($conn->connect_error) {
-//     die("Connection failed". $conn->connect_error);
-// }else{
-// echo"Connected Successfully";
-// }
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed". $conn->connect_error);
+}else{
+echo"Connected Successfully";
+}
 
 //the following code is Noah goofing off. you can delete at any time
 
+$sql = "CREATE TABLE people (
+id INT NOT NULL,
+
+)";
+
 ?>
-</html>
