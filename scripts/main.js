4600c8c10d0ca31f407af74368fb00aae220f466
NPorter-code <nathanielgaryporter@gmail.com>
2024-12-06 17:07:53 -0700

    my attempts to create a simple search engine

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+const sounds = [
+    {name: 'becky'}
+]
+const searchInput =document.querySelector('.input')
+
+searchInput.addEventListener("input", (e) => {
+    // Assigns the value of the event's target to a variable (whatever is typed into the search bar)
+    let value = e.target.value
+    
+    // Checks is input exists and if it is larger than zero
+    if (value && value.trim().length > 0){
+        // Refefines 'value' to exclude white space and changes input to all lowercase letters
+        value = value.trim().toLowerCase()
+        // Return the results only if the value of the search is included in the sound's display name
+        setList(sounds.filter(sound => {
+            return sound.name.includes(value)
+        }))
+    } else {
+        // returns nothing
+        // input is invalid -- show and error message or show no results
+    }
+}
+                             
+// showing results on a page:
+function setList(results){
+    
+    for (const sound of results){
+        // creates a li element for each result item
+        const resultItem = document.createElement('li')
+        
+        //adding a class to each item of the results
+        resultItem.classList.add('result-item')
+    
+        // grabbing the name of the current point of the loop and adding the name as the list item's text
+        const text = document.createTextNode(sounds.name)
+
+        // appending the text to the result item
+        resultItem.appendChild(text)
+
+        // appending the result item to the list
+        list.appendChild(resultItem)
+    }
+}
