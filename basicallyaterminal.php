d3e0c6a719a65abca3b55ab7e1b55c20a106927c
NoahZS-webdev <noahzsisson@gmail.com>
2024-12-18 10:16:00 -0800

    php edits

--- basicallyaterminal.php
+++ basicallyaterminal.php
@@ -14,6 +14,7 @@ $sName = $_POST["sName"];
 $author = $_POST["author"];
 $fileType = $_POST["fileType"];
 
+# echo what php got as form values (should be user input from form)
 echo "Got: <br>";
 echo "sName: ". $sName . '<br>' ."author: ". $author . '<br>' ."fileType: ". $fileType . "<br>";
 
@@ -42,8 +43,13 @@ echo"Connected Successfully<br><br>";
 //     )";
 
 // $sql = "SELECT * FROM audioData";
+if ($sName === "" and $author === "") { # if sName and author === nothing dont put into database
+    echo "YOU SUCK LOSERRRRRRRRRR";
+} else { # put data into database
 $sql = "INSERT INTO audioData (dispName, author, fileType)
 VALUES ('$sName', '$author', '$fileType')";
+}
+
 
 if ($conn->query($sql) === TRUE) {
     echo "Some shit was successful idk man i just work here";
@@ -51,8 +57,28 @@ if ($conn->query($sql) === TRUE) {
     echo "Oh wow i sure hope my sql works. Fuck you. said the sql who was actually the fuck you guy.<br><br>Error: " . $sql . "<br>" . $conn->error;
 }
 
+$sql = "SELECT fileID, dispName, author, fileType FROM audioData";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    //output the data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["fileID"] ." - Sound Name: " . $row["dispName"] . " - author: ". $row["author"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
 $conn->close();
 ?>
+
+<!-- get user input and set sqlk -->
+
+<!-- <form action="basicallyaaterminal.php" method="POST">
+    <textarea name="sqlUserInp" rows="3" cols="20">tee hee</textarea>
+</form>
+
+ -->
+
 </body>
 </html>
